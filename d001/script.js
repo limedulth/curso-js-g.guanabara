a329ef6da8dd332a589fce03a24acef966079d03
limedulth <julliosantana900@gmail.com>
2024-11-03 22:05:33 -0300

    Initial commit

---
+++ d001/script.js
@@ -0,0 +1,11 @@
+
+
+btnPrimeiro.onclick = function () {
+    alert('Você clicou no 1º botão')
+}
+btnSegundo.onclick = function () {
+    alert('Você clicou no 2º botão')
+}
+btnTerceiro.onclick = function () {
+    alert('Você clicou no 3º botão')
+}
